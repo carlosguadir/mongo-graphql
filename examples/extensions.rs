cf5a2593167ed9117b25915016c8bb66d47c8d8d
Carlos Guadir <carlosazzamies@gmail.com>
2026-08-10 09:15:19 -0500

    feat: enable DataLoader in example configurations

--- examples/extensions.rs
+++ examples/extensions.rs
@@ -155,6 +155,7 @@ async fn build_schema() -> Result<async_graphql::dynamic::Schema, Error> {
 
     let config = RuntimeConfig {
         max_page_size,
+        enable_dataloader: true,
     };
     let schema = SchemaBuilder::new(&config, &definition)
         .build(client, db, Some(vec![Box::new(ResolverLogger)]))
